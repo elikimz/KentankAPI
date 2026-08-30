ff5e6e8b2b9ffda6ab0fb384b0641dbb6928beda
elikimz <1.39633468e+08+elikimz@users.noreply.github.com>
2026-08-30 13:16:27 +0000

    Allow new Kentank domain in CORS

--- app/core/config.py
+++ app/core/config.py
@@ -29,6 +29,8 @@ class Settings(BaseSettings):
             'https://kentankclient.vercel.app',
             'https://kentankltkenya.online',
             'https://www.kentankltkenya.online',
+            'https://kentankmanufacturerke.online',
+            'https://www.kentankmanufacturerke.online',
         ]
         return list(dict.fromkeys(configured + required_frontends))
 
